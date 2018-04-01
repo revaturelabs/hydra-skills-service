31e0b041d80d2c69369991be6c6b9d0803da538b
Bryton Hall <email@bryton.io>
2018-04-01 14:21:14 -0400

    update dependencies and configuration for PCF services

--- src/main/java/com/revature/hydra/skills/SkillServiceApplication.java
+++ src/main/java/com/revature/hydra/skills/SkillServiceApplication.java
@@ -3,7 +3,7 @@ package com.revature.hydra.skills;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -13,9 +13,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableSwagger2
-@EntityScan("com.revature.beans")
 public class SkillServiceApplication {
 
 	public static void main(String[] args) {
